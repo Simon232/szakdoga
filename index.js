8cbf4a3fcb6bfa155db704d96f094b4126239e4c
weeez <xeropax@gmail.com>
2016-02-16 21:06:46 +0100

    chat done

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 /*var socket = new WebSocket('ws://localhost');
 socket.binaryType = 'arraybuffer';
 socket.send(new ArrayBuffer);*/
 
+app.use(express.static('public'));
 app.get('/', function(req, res){
-    res.sendfile('index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    //console.log('a user connected');
-   /* socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });*/
-
+    socket.broadcast.emit('hi');
+    console.log('a user connected');
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', function(){
+        console.log('user disconnect');
     });
 });
 
