12c8bd61b00e24cdc408b02edd892cfd9ba10889
weeez <xeropax@gmail.com>
2016-02-17 20:26:04 +0100

    threeJs cubed

--- public/js/program.js
+++ public/js/program.js
@@ -12,41 +12,68 @@ document.body.appendChild(renderer.domElement);
 
 var PI = 3.14;
 
+/*
+ var line_geo = new THREE.BoxGeometry(1000, 0.1, 0.1);
+ var line_geo2 = new THREE.BoxGeometry(0.1, 1000, 0.1);
+ var line_geo3 = new THREE.BoxGeometry(0.1, 0.1, 1000);
+
+ var basic_geometry = new THREE.BoxGeometry(5, 5, 0);
+ var geometry = new THREE.BoxGeometry(1, 1, 1);
+ var geometry2 = new THREE.BoxGeometry(2, 4, 0.4);
+
+ var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+ var material2 = new THREE.MeshBasicMaterial({color: 0xff0000});
+ var basic_material = new THREE.MeshBasicMaterial({color: 0x0000ff});
+
+ var line_material = new THREE.MeshBasicMaterial({color: 0x00006633}); //zöld
+ var line2_material = new THREE.MeshBasicMaterial({color: 0x00990000}); //piros
+ var line3_material = new THREE.MeshBasicMaterial({color: 0x00003399}); //kék
+
+ var cube = new THREE.Mesh(geometry, material);
+ var cube2 = new THREE.Mesh(geometry2, material2);
+ var floor = new THREE.Mesh(basic_geometry, basic_material);
+ var x_line = new THREE.Mesh(line_geo, line_material);
+ var y_line = new THREE.Mesh(line_geo2, line2_material);
+ var z_line = new THREE.Mesh(line_geo3, line3_material);
+ scene.add(floor);
+ scene.add(cube);
+ scene.add(cube2);
+ scene.add(x_line); //zöld
+ scene.add(y_line); //piros
+ scene.add(z_line); //kék
+ */
 
 var line_geo = new THREE.BoxGeometry(1000, 0.1, 0.1);
 var line_geo2 = new THREE.BoxGeometry(0.1, 1000, 0.1);
 var line_geo3 = new THREE.BoxGeometry(0.1, 0.1, 1000);
-
-var basic_geometry = new THREE.BoxGeometry(5, 5, 0);
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var geometry2 = new THREE.BoxGeometry(2, 4, 0.4);
-
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var material2 = new THREE.MeshBasicMaterial({color: 0xff0000});
-var basic_material = new THREE.MeshBasicMaterial({color: 0x0000ff});
-
 var line_material = new THREE.MeshBasicMaterial({color: 0x00006633}); //zöld
 var line2_material = new THREE.MeshBasicMaterial({color: 0x00990000}); //piros
 var line3_material = new THREE.MeshBasicMaterial({color: 0x00003399}); //kék
-
-var cube = new THREE.Mesh(geometry, material);
-var cube2 = new THREE.Mesh(geometry2, material2);
-var floor = new THREE.Mesh(basic_geometry, basic_material);
 var x_line = new THREE.Mesh(line_geo, line_material);
 var y_line = new THREE.Mesh(line_geo2, line2_material);
 var z_line = new THREE.Mesh(line_geo3, line3_material);
-scene.add(floor);
-scene.add(cube);
-scene.add(cube2);
-scene.add(x_line); //zöld
-scene.add(y_line); //piros
-scene.add(z_line); //kék
+
+var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+var cubeMaterial = new THREE.MeshBasicMaterial({color: 0x00FFFF33});
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
 
 var x = 0;
-var y = 1;
+var y = -2;
 var z = 0;
 
+var moveX = 0;
+var moveZ = 3;
+
+camera.position.x = 0;
+camera.position.y = 3;
 camera.position.z = 5;
+
+cube.position.x = 0.0;
+cube.position.y = 1.0;
+cube.position.z = 1.0;
+
+
 camera.lookAt(new THREE.Vector3(x, y, z));
 
 var def_cam_x = camera.position.x;
@@ -54,113 +81,69 @@ var def_cam_y = camera.position.y;
 var def_cam_z = camera.position.z;
 
 var buttonPressed = function () {
-    if (keyboard.pressed("d")) {
-        //console.log('d');
-        //x += 0.1;
-        //camera.position.x += 0.1;
-        x += 0.1;
-    }
-    if (keyboard.pressed("a")) {
-        //console.log('a');
-        //x -= 0.1;
-        //camera.position.x -= 0.1;
-        x -= 0.1;
-    }
-    if (keyboard.pressed("w")) {
-        //console.log('w');
-        //y += 0.1;
-        //camera.position.z -= 0.1;
-        if (x > 0 && y > 0 && z > 0) {
-            camera.position.z += (y / 100);
-            camera.position.y += y / 100;
-            camera.position.x += x / 100;
-        }
-        if (x == 0 && y > 0 && z == 0) {
-            camera.position.z -= (y / 100);
-            camera.position.y += y / 100;
-            camera.position.x += x / 100;
-            console.log(camera.position.x + ' ' + camera.position.y + ' ' + camera.position.z);
-        }
-        //z -= 0.1;
-    }
-    if (keyboard.pressed("s")) {
-        //console.log('s');
-        //y -= 0.1;
-        camera.position.z += 0.1;
-        z += 0.1;
+    if (keyboard.pressed("q")) {
+        cube.position.z += 0.1;
     }
     if (keyboard.pressed("e")) {
-
-        var fordulas = false;
-        if (!fordulas && camera.position.y < 4.9 && camera.position.z > 0.1) {
-            camera.position.y += 0.1;
-            camera.position.z -= 0.1;
-        } else {
-            fordulas = true;
-
-            camera.position.y -= 0.1;
-            camera.position.z -= 0.1;
-            if (fordulas && camera.position.y < 0.1 && camera.position.z > 4.9) {
-                fordulas = false;
-                camera.position.z = 5;
-                camera.position.y = 0;
-            }
-            if (fordulas && camera.position.y > -0.1 && camera.position.z < -4.9) {
-                fordulas = false;
-                camera.position.z = 5;
-                camera.position.y = 0;
-            }
-            if (fordulas && camera.position.y < -0.1 && camera.position.z < -4.9) {
-                fordulas = false;
-                camera.position.z = 5;
-                camera.position.y = 0;
-            }
-        }
-        //y += Math.sin(y*PI);
-
-        console.log(camera.position.y + ' ' + camera.position.z + ' ' + y);
+        cube.position.z -= 0.1;
     }
-    if (keyboard.pressed("q")) {
-        var fordulas = false;
-        if (!fordulas && camera.position.y > 0.1 && camera.position.z < 4.9) {
-            camera.position.y -= 0.1;
-            camera.position.z += 0.1;
-        } else {
-            fordulas = true;
 
-            camera.position.y -= 0.1;
-            camera.position.z -= 0.1;
-            if (fordulas && camera.position.z < 0.1 && camera.position.y > 4.9) {
-                fordulas = false;
-                camera.position.z = 0;
-                camera.position.y = 5;
-            }
-            if (fordulas && camera.position.z > -0.1 && camera.position.y < -4.9) {
-                fordulas = false;
-                camera.position.z = 5;
-                camera.position.y = 0;
-            }
-            if (fordulas && camera.position.y < -0.1 && camera.position.z < -4.9) {
-                fordulas = false;
-                camera.position.z = 5;
-                camera.position.y = 0;
-            }
 
-        }
-        console.log(camera.position.y + ' ' + camera.position.z + ' ' + y);
+    if (keyboard.pressed("a")) {
+        moveX -= 0.1;
+        cube.position.x -= 0.1;
     }
-    //keyboard.destroy();
+    if (keyboard.pressed("s")) {
+        moveZ += 0.1;
+        cube.position.z += 0.1;
+    }
+
+
+    if (keyboard.pressed("w")) {
+        moveZ -= 0.1;
+        cube.position.z -= 0.1;
+    }
+    if (keyboard.pressed("d")) {
+        moveX += 0.1;
+        cube.position.x += 0.1;
+    }
+
+
+    if (keyboard.pressed("t")) {
+        cube.position.y -= 0.1;
+    }
+    if (keyboard.pressed("g")) {
+        cube.position.y += 0.1;
+    }
+
     if (keyboard.pressed("escape")) {
         x = 0;
-        y = 1;
+        y = -2;
         z = 0;
-        camera.position.z = 5;
-        camera.position.y = 0;
-        camera.position.x = 0;
-    }
-    camera.lookAt(new THREE.Vector3(x, y, z));
-}
 
+        camera.position.x = 0;
+        camera.position.y = 3;
+        camera.position.z = 5;
+
+        cube.position.x = 0.0;
+        cube.position.y = 1.0;
+        cube.position.z = 1.0;
+
+        moveX = 0;
+        moveZ = 3;
+        camera.lookAt(new THREE.Vector3(x, y, z))
+    }
+    //camera.lookAt(new THREE.Vector3(x, y, z))
+    camera.position.x = moveX;
+    camera.position.z = moveZ;
+
+};
+
+
+scene.add(cube);
+scene.add(x_line); //zöld
+scene.add(y_line); //piros
+scene.add(z_line); //kék
 
 var render = function () {
     requestAnimationFrame(render);
