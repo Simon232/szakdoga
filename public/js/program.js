34142573007f8ec1745584e7574aff94b6bc8ea6
weeez <xeropax@gmail.com>
2016-03-08 23:07:20 +0100

    optimalizing

--- public/js/program.js
+++ public/js/program.js
@@ -65,7 +65,7 @@ var init = function () {
     var texture = new THREE.TextureLoader().load("pics/sand3.jpg");
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(gameWidth / 4, gameWidth / 4);
+    texture.repeat.set(gameWidth / 12, gameWidth / 12);
 
     var ground_geo = new THREE.BoxGeometry(gameWidth, 0.0001, gameWidth);
     //var cube_material = new THREE.MeshPhongMaterial( { ambient: 0x050505, color: 0x0033ff, specular: 0x555555, shininess: 30 } );
@@ -162,29 +162,6 @@ socket.on('new', function (msg) {
         }
     }
 });
-
-function decimalAdjust(type, value, exp) {
-    // If the exp is undefined or zero...
-    if (typeof exp === 'undefined' || +exp === 0) {
-        return Math[type](value);
-    }
-    value = +value;
-    exp = +exp;
-    // If the value is not a number or the exp is not an integer...
-    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
-        return NaN;
-    }
-    // Shift
-    value = value.toString().split('e');
-    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
-    // Shift back
-    value = value.toString().split('e');
-    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
-}
-
-Math.round10 = function (value, exp) {
-    return decimalAdjust('round', value, exp);
-};
 
 
 socket.on('move', function (obj) {
