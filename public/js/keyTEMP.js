73bf779a4849b3147654bf6c533c530834d83d42
weeez <xeropax@gmail.com>
2016-03-15 12:20:01 +0100

    fixed gamewidth with moving

--- public/js/keyTEMP.js
+++ public/js/keyTEMP.js
@@ -2,6 +2,201 @@
  y = cy + r * sin(a)
  * */
 
+/*
+*
+ /*
+ var map = {
+ 37: false, // left
+ 38: false, // up
+ 39: false, // right
+ 40: false, // down
+ 87: false, // W
+ 83: false, // S
+ 65: false, // A
+ 68: false, // D
+ 27: false, // ESC
+ 81: false, // Q
+ 69: false, // E
+ 80: false, // P
+ 82: false, // R
+ 70: false  // F
+ };
+ jQuery(document).keydown(function (e) {
+ var prevent = true;
+ // Update the state of the attached control to "true"
+
+ if (e.keyCode in map) {
+ map[e.keyCode] = true;
+
+
+
+
+ if (map[87]) { // W
+ if (!pause &&
+ (obj.socketCube.z - movingSpeed) > (-1 * gameWidth) / 2 + cubeHalf) {
+
+ obj.socketCube.z -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+ obj.socketCube.x -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+
+ obj.camera.z = cubes[thisSocket].position.z + 6 * (Math.cos( cubes[thisSocket].rotation.y));
+ obj.camera.x = cubes[thisSocket].position.x + 6 * (Math.sin( cubes[thisSocket].rotation.y));
+
+ changeScene();
+ } else {
+ console.log(gameWidth);
+ }
+ //return;
+ }
+ if (map[83]) { // S
+ if (!pause
+ && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
+
+ obj.socketCube.z += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+ obj.socketCube.x += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+
+ obj.camera.z = cubes[thisSocket].position.z + 6 * (Math.cos( cubes[thisSocket].rotation.y));
+ obj.camera.x = cubes[thisSocket].position.x + 6 * (Math.sin( cubes[thisSocket].rotation.y));
+
+ changeScene();
+ }
+ //return;
+ }
+ if (map[65]) { // A
+ if (!pause
+ && (obj.socketCube.x - movingSpeed) > -gameWidth / 2 + cubeHalf) {
+ obj.camera.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+ obj.camera.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+
+
+ obj.socketCube.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+ obj.socketCube.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+
+ changeScene();
+ }
+ //return;
+ }
+ if (map[68]) { // D
+ if (!pause
+ && (obj.socketCube.x + movingSpeed) < gameWidth / 2 - cubeHalf) {
+ obj.camera.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+ obj.camera.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+
+
+ obj.socketCube.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+ obj.socketCube.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+
+ changeScene();
+ }
+ //return;
+ }
+ if (map[87] && map[68]) { // W + D
+ if (!pause
+ && (obj.socketCube.z - movingSpeed) > -gameWidth / 2 + cubeHalf) {
+
+ cubes[thisSocket].rotation.y -= rotationSpeed;
+ socket.emit('rotation', {
+ sid: thisSocket,
+ rotY: -rotationSpeed,
+ room: thisRoom
+ }
+ );
+
+ changeScene();
+ }
+ //return;
+ }
+
+ if (map[83] && map[68]) { // S + D
+ if (!pause
+ && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
+
+ cubes[thisSocket].rotation.y += rotationSpeed;
+ socket.emit('rotation', {
+ sid: thisSocket,
+ rotY: rotationSpeed,
+ room: thisRoom
+ }
+ );
+
+ changeScene();
+ }
+ //return;
+ }
+
+ if (map[87] && map[65]) { // W + A
+ if (!pause
+ && (obj.socketCube.z - movingSpeed) > -gameWidth / 2 + cubeHalf) {
+ cubes[thisSocket].rotation.y += rotationSpeed;
+ socket.emit('rotation', {
+ sid: thisSocket,
+ rotY: rotationSpeed,
+ room: thisRoom
+ }
+ );
+
+ changeScene();
+ }
+ //return;
+ }
+
+ if (map[83] && map[65]) { // S + A
+ if (!pause
+ && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
+ cubes[thisSocket].rotation.y -= rotationSpeed;
+ socket.emit('rotation', {
+ sid: thisSocket,
+ rotY: -rotationSpeed,
+ room: thisRoom
+ }
+ );
+
+ changeScene();
+ }
+ //return;
+ }
+ if (map[27]) { // ESC
+ if (!pause) {
+ obj.camera.x = 0;
+ obj.camera.y = 4;
+ obj.camera.z = 6;
+
+ obj.socketCube.x = 0.0;
+ obj.socketCube.y = 0.5;
+ obj.socketCube.z = 1.0;
+ obj.rotY = 0.0;
+ cubes[thisSocket].rotation.y = 0.0;
+
+ socket.emit('reset', {
+ sid: thisSocket,
+ rotY: 0.0,
+ room: thisRoom
+ });
+
+ changeScene();
+ }
+ //return;
+ }
+ if (map[80]) {
+ pause = !pause;
+ //return;
+ } else {
+ prevent = false;
+ }
+
+ }
+
+ if (prevent) {
+ e.preventDefault();
+ } else {
+ return;
+ }
+ }).keyup(function (e) {
+ if (e.keyCode in map) {
+ map[e.keyCode] = false;
+ obj.socketCube.rotY = 0.0;
+ }
+ });
+ */
 
 
 /*
--- public/js/keymapping.js
+++ public/js/keymapping.js
@@ -2,6 +2,7 @@ var PI = Math.PI;
 var movingSpeed = 0.05;
 var rotationSpeed = PI / 160;
 var cubeHalf = 0.49;
+var cameraDistance = 6;
 
 var keyPressed = {};
 
@@ -15,66 +16,76 @@ document.addEventListener('keyup', function (e) {
 function gameLoop() {
 
     if (keyPressed[87]) { // W
-        if (!pause &&
-            (obj.socketCube.z - movingSpeed) > (-1 * gameWidth) / 2 + cubeHalf) {
+        if (!pause) {
+            if (obj.socketCube.z - Math.cos(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.z - Math.cos(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf) {
+                obj.socketCube.z -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
+            if (obj.socketCube.x - Math.sin(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.x - Math.sin(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf) {
+                obj.socketCube.x -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
 
-            obj.socketCube.z -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-            obj.socketCube.x -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-
-            obj.camera.z = cubes[thisSocket].position.z + 6 * (Math.cos(cubes[thisSocket].rotation.y));
-            obj.camera.x = cubes[thisSocket].position.x + 6 * (Math.sin(cubes[thisSocket].rotation.y));
+            obj.camera.z = cubes[thisSocket].position.z + cameraDistance * (Math.cos(cubes[thisSocket].rotation.y));
+            obj.camera.x = cubes[thisSocket].position.x + cameraDistance * (Math.sin(cubes[thisSocket].rotation.y));
 
             changeScene();
-        } else {
-            console.log(gameWidth);
         }
-        //return;
     }
     if (keyPressed[83]) { // S
-        if (!pause
-            && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
+        if (!pause) {
+            if (obj.socketCube.z + Math.cos(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.z + Math.cos(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf
+            ) {
+                obj.socketCube.z += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
+            if (obj.socketCube.x + Math.sin(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.x + Math.sin(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf) {
+                obj.socketCube.x += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
 
-            obj.socketCube.z += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-            obj.socketCube.x += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-
-            obj.camera.z = cubes[thisSocket].position.z + 6 * (Math.cos(cubes[thisSocket].rotation.y));
-            obj.camera.x = cubes[thisSocket].position.x + 6 * (Math.sin(cubes[thisSocket].rotation.y));
+            obj.camera.z = cubes[thisSocket].position.z + cameraDistance * (Math.cos(cubes[thisSocket].rotation.y));
+            obj.camera.x = cubes[thisSocket].position.x + cameraDistance * (Math.sin(cubes[thisSocket].rotation.y));
 
             changeScene();
         }
-        //return;
     }
     if (keyPressed[65]) { // A
-        if (!pause
-            && (obj.socketCube.x - movingSpeed) > -gameWidth / 2 + cubeHalf) {
-            obj.camera.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-            obj.camera.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-
-
-            obj.socketCube.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-            obj.socketCube.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+        if (!pause) {
+            if (obj.socketCube.z + Math.sin(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.z + Math.sin(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf) {
+                obj.socketCube.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+                obj.camera.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
+            if (obj.socketCube.x - Math.cos(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.x - Math.cos(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf) {
+                obj.socketCube.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+                obj.camera.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
 
             changeScene();
         }
-        //return;
     }
     if (keyPressed[68]) { // D
-        if (!pause
-            && (obj.socketCube.x + movingSpeed) < gameWidth / 2 - cubeHalf) {
-            obj.camera.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-            obj.camera.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-
-
-            obj.socketCube.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-            obj.socketCube.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-
+        if (!pause) {
+            if (obj.socketCube.z - Math.sin(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.z - Math.sin(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf) {
+                obj.socketCube.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+                obj.camera.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
+            if (obj.socketCube.x + Math.cos(cubes[thisSocket].rotation.y) * movingSpeed >= -(gameWidth / 2) + cubeHalf
+                && obj.socketCube.x + Math.cos(cubes[thisSocket].rotation.y) * movingSpeed <= (gameWidth / 2) - cubeHalf) {
+                obj.socketCube.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+                obj.camera.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
+            }
             changeScene();
         }
-        //return;
     }
     if (keyPressed[87] && keyPressed[68]) { // W + D
         if (!pause
-            && (obj.socketCube.z - movingSpeed) > -gameWidth / 2 + cubeHalf) {
+            && obj.socketCube.z <= (gameWidth / 2) - cubeHalf && obj.socketCube.z >= -(gameWidth / 2) + cubeHalf
+            && obj.socketCube.x <= (gameWidth / 2) - cubeHalf && obj.socketCube.x >= -(gameWidth / 2) + cubeHalf
+        ) {
 
             cubes[thisSocket].rotation.y -= rotationSpeed;
             socket.emit('rotation', {
@@ -86,12 +97,13 @@ function gameLoop() {
 
             changeScene();
         }
-        //return;
     }
 
     if (keyPressed[83] && keyPressed[68]) { // S + D
         if (!pause
-            && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
+            && obj.socketCube.z <= (gameWidth / 2) - cubeHalf && obj.socketCube.z >= -(gameWidth / 2) + cubeHalf
+            && obj.socketCube.x <= (gameWidth / 2) - cubeHalf && obj.socketCube.x >= -(gameWidth / 2) + cubeHalf
+        ) {
 
             cubes[thisSocket].rotation.y += rotationSpeed;
             socket.emit('rotation', {
@@ -103,12 +115,13 @@ function gameLoop() {
 
             changeScene();
         }
-        //return;
     }
 
     if (keyPressed[87] && keyPressed[65]) { // W + A
         if (!pause
-            && (obj.socketCube.z - movingSpeed) > -gameWidth / 2 + cubeHalf) {
+            && obj.socketCube.z <= (gameWidth / 2) - cubeHalf && obj.socketCube.z >= -(gameWidth / 2) + cubeHalf
+            && obj.socketCube.x <= (gameWidth / 2) - cubeHalf && obj.socketCube.x >= -(gameWidth / 2) + cubeHalf
+        ) {
             cubes[thisSocket].rotation.y += rotationSpeed;
             socket.emit('rotation', {
                     sid: thisSocket,
@@ -119,12 +132,13 @@ function gameLoop() {
 
             changeScene();
         }
-        //return;
     }
 
     if (keyPressed[83] && keyPressed[65]) { // S + A
         if (!pause
-            && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
+            && obj.socketCube.z <= (gameWidth / 2) - cubeHalf && obj.socketCube.z >= -(gameWidth / 2) + cubeHalf
+            && obj.socketCube.x <= (gameWidth / 2) - cubeHalf && obj.socketCube.x >= -(gameWidth / 2) + cubeHalf
+        ) {
             cubes[thisSocket].rotation.y -= rotationSpeed;
             socket.emit('rotation', {
                     sid: thisSocket,
@@ -135,7 +149,6 @@ function gameLoop() {
 
             changeScene();
         }
-        //return;
     }
     if (keyPressed[27]) { // ESC
         if (!pause) {
@@ -157,11 +170,10 @@ function gameLoop() {
 
             changeScene();
         }
-        //return;
+
     }
     if (keyPressed[80]) {
         pause = !pause;
-        //return;
     } else {
         prevent = false;
     }
@@ -173,197 +185,3 @@ function gameLoop() {
 }
 
 gameLoop();
-
-/*
- var map = {
- 37: false, // left
- 38: false, // up
- 39: false, // right
- 40: false, // down
- 87: false, // W
- 83: false, // S
- 65: false, // A
- 68: false, // D
- 27: false, // ESC
- 81: false, // Q
- 69: false, // E
- 80: false, // P
- 82: false, // R
- 70: false  // F
- };
- jQuery(document).keydown(function (e) {
- var prevent = true;
- // Update the state of the attached control to "true"
-
- if (e.keyCode in map) {
- map[e.keyCode] = true;
-
-
-
-
- if (map[87]) { // W
- if (!pause &&
- (obj.socketCube.z - movingSpeed) > (-1 * gameWidth) / 2 + cubeHalf) {
-
- obj.socketCube.z -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
- obj.socketCube.x -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-
- obj.camera.z = cubes[thisSocket].position.z + 6 * (Math.cos( cubes[thisSocket].rotation.y));
- obj.camera.x = cubes[thisSocket].position.x + 6 * (Math.sin( cubes[thisSocket].rotation.y));
-
- changeScene();
- } else {
- console.log(gameWidth);
- }
- //return;
- }
- if (map[83]) { // S
- if (!pause
- && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
-
- obj.socketCube.z += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
- obj.socketCube.x += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
-
- obj.camera.z = cubes[thisSocket].position.z + 6 * (Math.cos( cubes[thisSocket].rotation.y));
- obj.camera.x = cubes[thisSocket].position.x + 6 * (Math.sin( cubes[thisSocket].rotation.y));
-
- changeScene();
- }
- //return;
- }
- if (map[65]) { // A
- if (!pause
- && (obj.socketCube.x - movingSpeed) > -gameWidth / 2 + cubeHalf) {
- obj.camera.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
- obj.camera.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-
-
- obj.socketCube.z += Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
- obj.socketCube.x -= Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-
- changeScene();
- }
- //return;
- }
- if (map[68]) { // D
- if (!pause
- && (obj.socketCube.x + movingSpeed) < gameWidth / 2 - cubeHalf) {
- obj.camera.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
- obj.camera.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-
-
- obj.socketCube.z -= Math.sin(cubes[thisSocket].rotation.y) * movingSpeed;
- obj.socketCube.x += Math.cos(cubes[thisSocket].rotation.y) * movingSpeed;
-
- changeScene();
- }
- //return;
- }
- if (map[87] && map[68]) { // W + D
- if (!pause
- && (obj.socketCube.z - movingSpeed) > -gameWidth / 2 + cubeHalf) {
-
- cubes[thisSocket].rotation.y -= rotationSpeed;
- socket.emit('rotation', {
- sid: thisSocket,
- rotY: -rotationSpeed,
- room: thisRoom
- }
- );
-
- changeScene();
- }
- //return;
- }
-
- if (map[83] && map[68]) { // S + D
- if (!pause
- && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
-
- cubes[thisSocket].rotation.y += rotationSpeed;
- socket.emit('rotation', {
- sid: thisSocket,
- rotY: rotationSpeed,
- room: thisRoom
- }
- );
-
- changeScene();
- }
- //return;
- }
-
- if (map[87] && map[65]) { // W + A
- if (!pause
- && (obj.socketCube.z - movingSpeed) > -gameWidth / 2 + cubeHalf) {
- cubes[thisSocket].rotation.y += rotationSpeed;
- socket.emit('rotation', {
- sid: thisSocket,
- rotY: rotationSpeed,
- room: thisRoom
- }
- );
-
- changeScene();
- }
- //return;
- }
-
- if (map[83] && map[65]) { // S + A
- if (!pause
- && (obj.socketCube.z + movingSpeed) < gameWidth / 2 - cubeHalf) {
- cubes[thisSocket].rotation.y -= rotationSpeed;
- socket.emit('rotation', {
- sid: thisSocket,
- rotY: -rotationSpeed,
- room: thisRoom
- }
- );
-
- changeScene();
- }
- //return;
- }
- if (map[27]) { // ESC
- if (!pause) {
- obj.camera.x = 0;
- obj.camera.y = 4;
- obj.camera.z = 6;
-
- obj.socketCube.x = 0.0;
- obj.socketCube.y = 0.5;
- obj.socketCube.z = 1.0;
- obj.rotY = 0.0;
- cubes[thisSocket].rotation.y = 0.0;
-
- socket.emit('reset', {
- sid: thisSocket,
- rotY: 0.0,
- room: thisRoom
- });
-
- changeScene();
- }
- //return;
- }
- if (map[80]) {
- pause = !pause;
- //return;
- } else {
- prevent = false;
- }
-
- }
-
- if (prevent) {
- e.preventDefault();
- } else {
- return;
- }
- }).keyup(function (e) {
- if (e.keyCode in map) {
- map[e.keyCode] = false;
- obj.socketCube.rotY = 0.0;
- }
- });
- */
